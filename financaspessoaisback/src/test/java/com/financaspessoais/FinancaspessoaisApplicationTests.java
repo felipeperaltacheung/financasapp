591a13cb51d70e064dd282b8992627a49b46a769
felipeperaltacheung <f33___@hotmail.com>
2023-02-25 15:34:43 -0300

    subindo backend spring

---
+++ financaspessoaisback/src/test/java/com/financaspessoais/FinancaspessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financaspessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancaspessoaisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
