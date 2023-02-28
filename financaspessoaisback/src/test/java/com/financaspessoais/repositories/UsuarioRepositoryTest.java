e399fb3442bcb1afa3b4f76162d69ff49fdbdc0e
felipeperaltacheung <f33___@hotmail.com>
2023-02-27 23:24:40 -0300

    refatorando o teste do usuarioReporitory p usar @DataJpaTest e @AutoConfigureTestDatabase, usando um TestEntityManager

--- financaspessoaisback/src/test/java/com/financaspessoais/repositories/UsuarioRepositoryTest.java
+++ financaspessoaisback/src/test/java/com/financaspessoais/repositories/UsuarioRepositoryTest.java
@@ -5,24 +5,30 @@ import org.junit.Test;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.assertj.core.api.Assertions;
 
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @RunWith(SpringRunner.class)
 public class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    TestEntityManager testEntityManager;
+
     @Test
     public void deveVerificarAexistenciaDeUmEmail(){
 
         //cenario
         Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        usuarioRepository.save(usuario);
+        testEntityManager.persist(usuario);
         
         
         //acao
