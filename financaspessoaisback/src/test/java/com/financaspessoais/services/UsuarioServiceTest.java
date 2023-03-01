b16e8b43f780ae1efba1e79584526adedf1e7b69
felipeperaltacheung <f33___@hotmail.com>
2023-02-28 21:53:46 -0300

    Inserindo teste de autenticacao

--- financaspessoaisback/src/test/java/com/financaspessoais/services/UsuarioServiceTest.java
+++ financaspessoaisback/src/test/java/com/financaspessoais/services/UsuarioServiceTest.java
@@ -1,14 +1,17 @@
 package com.financaspessoais.services;
 
 import com.financaspessoais.exceptions.RegraNegocioException;
+import com.financaspessoais.model.entities.Usuario;
 import com.financaspessoais.repositories.UsuarioRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-
+import org.assertj.core.api.Assertions;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 public class UsuarioServiceTest {
@@ -20,6 +23,23 @@ public class UsuarioServiceTest {
     @Before
     public void setup(){
         usuarioService = new UsuarioServiceImplementation(usuarioRepository);
+    }
+
+    @Test(expected = Test.None.class)
+    public void deveAutenticarUsuarioComSucesso(){
+        //cenario
+        String email = "email@email.com";
+        String senha = "senha";
+
+        Usuario usuario = Usuario.builder().email(email).senha(senha).id(1L).build();
+        Mockito.when(usuarioRepository.findByEmail(email)).thenReturn(Optional.of(usuario));
+
+        //acao
+        Usuario result = usuarioService.autenticar(email, senha);
+
+        //verificacao
+        Assertions.assertThat(result).isNotNull();
+
     }
 
     @Test(expected = Test.None.class) //para nao lancar exception
