273ecf963a3a51a0c922cc2e0c4f82e729f7578b
felipeperaltacheung <f33___@hotmail.com>
2023-02-28 21:39:52 -0300

    refatorando o test da service do usuario, com mocks

--- financaspessoaisback/src/test/java/com/financaspessoais/services/UsuarioServiceTest.java
+++ financaspessoaisback/src/test/java/com/financaspessoais/services/UsuarioServiceTest.java
@@ -1,41 +1,40 @@
 package com.financaspessoais.services;
 
 import com.financaspessoais.exceptions.RegraNegocioException;
-import com.financaspessoais.model.entities.Usuario;
 import com.financaspessoais.repositories.UsuarioRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplicationRunListener;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 public class UsuarioServiceTest {
 
-    @Autowired
     UsuarioService usuarioService;
-    @Autowired
     UsuarioRepository usuarioRepository;
+
+    @Before
+    public void setup(){
+        usuarioRepository = Mockito.mock(UsuarioRepository.class);
+        usuarioService = new UsuarioServiceImplementation(usuarioRepository);
+    }
 
     @Test(expected = Test.None.class) //para nao lancar exception
     public void deveValidarEmail(){
 
         //cenario
-        usuarioRepository.deleteAll();
+        Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
 
         //acao
         usuarioService.validarEmail("email@email.com");
-
-
     }
 
     @Test(expected = RegraNegocioException.class)
     public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
 
-        Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-        usuarioRepository.save(usuario);
+        Mockito.when(usuarioRepository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
         usuarioService.validarEmail("email@email.com");
     }
